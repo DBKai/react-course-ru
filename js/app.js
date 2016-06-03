4ddaa2c7c885a95b486a310876b617cacbea4d45
Dmurkay <dbkai@ya.ru>
2016-06-03 23:09:14 +0500

    commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+/*
+console.log(React);
+console.log(ReactDOM);*/
+
+/*
+ReactDOM.render(
+  React.createElement('h1', null, 'Hello'),
+  document.getElementById('root')
+);*/
+
+ReactDOM.render(
+  <h1>Hello</h1>,
+  document.getElementById('root')
+);
