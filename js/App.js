4ec32c8b37c83f43664bc1457905575ccb1edfa3
Dmurkay <dbkai@ya.ru>
2016-06-06 17:35:01 +0500

    using state

--- js/App.js
+++ js/App.js
@@ -1,32 +1,63 @@
 var my_news = [
   {
     author: 'Sasha Pechkin',
-    text: 'In thursday four number...'
+    text: 'In thursday four number...',
+    bigText: 'four and a quarter hours four black devils drew a black ink drawing.'
   },
   {
     author: 'Prosto Vasya',
-    text: 'I think that dollar should cost 35 rubles!'
+    text: 'I think that dollar should cost 35 rubles!',
+    bigText: 'And euro 42!'
   },
   {
     author: 'Guest',
-    text: 'Free. Download. The best site - http://localhost:3000'
+    text: 'Free. Download. The best site - http://localhost:3000',
+    bigText: 'Actually a fee, just need to read the license agreement very long.'
   }
 ];
 
 var Article = React.createClass({
+  propTypes: {
+    data: React.PropTypes.shape({
+      author: React.PropTypes.string.isRequired,
+      text: React.PropTypes.string.isRequired,
+      bigText: React.PropTypes.string.isRequired
+    })
+  },
+
+  getInitialState: function () {
+    return {
+      visible: false
+    }
+  },
+
+  readMoreClick: function (e) {
+    e.preventDefault();
+    this.setState({
+      visible: true
+    });
+  },
+
   render: function () {
-    var { author, text } = this.props.data;
+    var {author, text, bigText} = this.props.data,
+      {visible} = this.state;
 
     return (
       <div className="article">
         <p className="news__author">{author}:</p>
         <p className="news__text">{text}</p>
+        <a href="#" onClick={this.readMoreClick} className={'news__readmore ' + (visible ? 'none' : '')}>Read more</a>
+        <p className={'news__big-text ' + (visible ? '' : 'none')}>{bigText}</p>
       </div>
     )
   }
 });
 
 var News = React.createClass({
+  propTypes: {
+    data: React.PropTypes.array.isRequired
+  },
+
   render: function () {
     var data = this.props.data;
     var newsCount = data.length;
